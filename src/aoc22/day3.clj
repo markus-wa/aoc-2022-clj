b0ef3ffbe6d213be0ea951e4695a0719eddbedb9
Markus Walther <mark@saiko.tech>
2022-12-04 20:56:52 +0000

    more day 3 cleanup

--- src/aoc22/day3.clj
+++ src/aoc22/day3.clj
@@ -1,22 +1,17 @@
 (ns aoc22.day3
   (:require [clojure.string :as str]
             [clojure.set :as set]
-            [clojure.java.io :as io]
-            [medley.core :as m]))
-
-(defn ->compartments [s]
-  (->> (partition (/ (count s) 2) s)
-       (map set)))
+            [clojure.java.io :as io]))
 
 (def item->priority
   (->> (concat (range (int \a) (inc (int \z))) (range (int \A) (inc (int \Z))))
        vec
        (zipmap (iterate inc 1))
-       (m/map-kv (fn [k v] [(char v) k]))))
+       set/map-invert))
 
 (defn part-1 [s]
   (->> (str/split-lines s)
-       (map ->compartments)
+       (map #(split-at (/ (count %) 2) %))
        (map #(set/intersection (first %) (second %)))
        (map #(item->priority (first %)))
        (reduce +)))
@@ -25,7 +20,7 @@
   (->> (str/split-lines s)
        (map set)
        (partition 3)
-       (map #(set/intersection (first %) (second %) (last %)))
+       (map (fn [[a b c]] (set/intersection a b c)))
        (map #(item->priority (first %)))
        (reduce +)))
 
