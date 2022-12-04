c23197eeed09660adf2bbc847d4e70b2805c0349
Markus Walther <mark@saiko.tech>
2022-12-04 20:12:23 +0000

    clean up day 2

--- src/aoc22/day2.clj
+++ src/aoc22/day2.clj
@@ -2,10 +2,6 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(def test-input "A Y
-B X
-C Z")
-
 (defn points-win-draw-loss
   [op me]
   (if (= op me)
@@ -18,21 +14,6 @@ C Z")
 (defn points
   [[op me]]
   (+ (points-win-draw-loss op me) me))
-
-(defn part-1 [s]
-  (->> (str/split s #"\n")
-       (map #(str/split % #" "))
-       (map #(map {"A" 1 "B" 2 "C" 3 "X" 1 "Y" 2 "Z" 3} %))
-       (map points)
-       (reduce +)))
-
-(part-1 test-input) ; want 15
-
-(def input
-  (-> (io/resource "day2.txt")
-      slurp))
-
-(part-1 input)
 
 (defn points-2
   [[op result]]
@@ -51,12 +32,25 @@ C Z")
              "Y" 3
              "Z" 1))))
 
-(defn part-2 [s]
-  (->> (str/split s #"\n")
-       (map #(str/split % #" "))
-       (map points-2)
-       (reduce +)))
+(defn run [s]
+  (let [strategy (->> (str/split s #"\n")
+                      (map #(str/split % #" ")))]
+    [(->> (map #(map {"A" 1 "B" 2 "C" 3 "X" 1 "Y" 2 "Z" 3} %) strategy)
+          (map points)
+          (reduce +))
+     (->> (map points-2 strategy)
+          (reduce +))]))
 
-(part-2 test-input) ; want 12
+(def input
+  (-> (io/resource "day2.txt")
+      slurp))
 
-(part-2 input)
+(run input)
+
+(comment
+  (def test-input "A Y
+B X
+C Z")
+
+  (run test-input) ; want [15 12]
+  )
